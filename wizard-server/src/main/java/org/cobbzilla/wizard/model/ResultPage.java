b37056e3cfb060ea75751291c88ca876343b3739
jonathan <jonathan@kyuss.org>
2013-05-05 02:36:07 -0700

    a bit more useful

--- wizard-server/src/main/java/org/cobbzilla/wizard/model/ResultPage.java
+++ wizard-server/src/main/java/org/cobbzilla/wizard/model/ResultPage.java
@@ -2,38 +2,48 @@ package org.cobbzilla.wizard.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.cobbzilla.util.string.StringUtil;
 import org.cobbzilla.wizard.validation.ValidEnum;
 
+@NoArgsConstructor
 public class ResultPage {
 
     public static final int MAX_FILTER_LENGTH = 50;
     public static final int MAX_SORTFIELD_LENGTH = 50;
 
     public enum SortOrder { ASC, DESC }
+    public static final String DEFAULT_SORT = SortOrder.DESC.name();
 
     public static final ResultPage DEFAULT_PAGE = new ResultPage();
     public static final ResultPage FIRST_RESULT = new ResultPage(1, 1, null, null, null);
 
-    public ResultPage () {}
+    public ResultPage (int pageNumber, int pageSize, String sortField, SortOrder sortOrder) {
+        this(pageNumber, pageSize, sortField, sortOrder, null);
+    }
 
     public ResultPage (int pageNumber, int pageSize, String sortField, SortOrder sortOrder, String filter) {
         this.pageNumber = pageNumber;
         this.pageSize = pageSize;
         this.sortField = sortField;
-        this.sortOrder = (sortOrder == null) ? null : sortOrder.name();
+        this.sortOrder = (sortOrder == null) ? ResultPage.DEFAULT_SORT : sortOrder.name();
         this.filter = filter;
     }
 
-    @Getter @Setter
-    private int pageNumber = 1;
+    public static ResultPage singleResult (String sortField, SortOrder sortOrder) {
+        return new ResultPage(1, 1, sortField, sortOrder);
+    }
 
-    @JsonIgnore
-    public int getPageOffset () { return pageNumber * pageSize; }
+    public static ResultPage singleResult (String sortField, SortOrder sortOrder, String filter) {
+        return new ResultPage(1, 1, sortField, sortOrder, filter);
+    }
 
-    @Getter @Setter
-    private int pageSize = 10;
+    @Getter @Setter private int pageNumber = 1;
+
+    @JsonIgnore public int getPageOffset () { return (pageNumber-1) * pageSize; }
+
+    @Getter @Setter private int pageSize = 10;
 
     @Setter
     private String sortField = "ctime";
@@ -41,20 +51,18 @@ public class ResultPage {
         // only return the first several chars, to thwart a hypothetical injection attack.
         return StringUtil.prefix(sortField, MAX_SORTFIELD_LENGTH);
     }
+    @JsonIgnore public boolean getHasSortField () { return sortField != null; }
 
     @ValidEnum(type=SortOrder.class, emptyOk=true, message= BasicConstraintConstants.ERR_SORT_ORDER_INVALID)
-    @Getter @Setter
-    private String sortOrder = SortOrder.DESC.name();
+    @Getter @Setter private String sortOrder = ResultPage.DEFAULT_SORT;
 
-    @JsonIgnore
-    public SortOrder getSortType () { return sortOrder == null ? null : SortOrder.valueOf(sortOrder); }
+    @JsonIgnore public SortOrder getSortType () { return sortOrder == null ? null : SortOrder.valueOf(sortOrder); }
 
-    @Setter
-    private String filter = null;
+    @Setter private String filter = null;
     public String getFilter() {
         // only return the first several chars, to thwart a hypothetical injection attack.
         return StringUtil.prefix(filter, MAX_FILTER_LENGTH);
     }
-    public boolean getHasFilter() { return filter != null && filter.trim().length() > 0; }
+    @JsonIgnore public boolean getHasFilter() { return filter != null && filter.trim().length() > 0; }
 
 }
