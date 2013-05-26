d708951c5e999893147e5997047b8bdd6512b760
jonathan <jonathan@kyuss.org>
2013-05-26 03:16:45 -0700

    make AbstractResourceIT a WizardClient, reuse client code

--- wizard-client/src/main/java/org/cobbzilla/wizard/client/WizardClient.java
+++ wizard-client/src/main/java/org/cobbzilla/wizard/client/WizardClient.java
@@ -1,14 +1,16 @@
 package org.cobbzilla.wizard.client;
 
 import lombok.AllArgsConstructor;
+import lombok.Cleanup;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpPut;
-import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.client.methods.*;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -17,30 +19,35 @@ import org.cobbzilla.wizard.util.RestResponse;
 import java.io.IOException;
 import java.io.InputStream;
 
-@Slf4j @AllArgsConstructor
+@Slf4j @NoArgsConstructor @AllArgsConstructor
 public class WizardClient {
 
-    private String baseUri;
+    @Getter private String baseUri;
 
-    public RestResponse doGet(String path) throws Exception {
-        HttpClient client = new DefaultHttpClient();
-        HttpGet httpGet = new HttpGet(baseUri+path);
+    protected HttpClient getHttpClient () { return new DefaultHttpClient(); }
+
+    protected RestResponse doGet(String path) throws Exception {
+        HttpClient client = getHttpClient();
+        final String url = getUrl(path, getBaseUri());
+        @Cleanup("releaseConnection") HttpGet httpGet = new HttpGet(url);
         return getResponse(client, httpGet);
     }
 
-    public RestResponse doPost(String path, String json) throws Exception {
-        HttpClient client = new DefaultHttpClient();
-        HttpPost httpPost = new HttpPost(baseUri+path);
+    protected RestResponse doPost(String path, String json) throws Exception {
+        HttpClient client = getHttpClient();
+        final String url = getUrl(path, getBaseUri());
+        @Cleanup("releaseConnection") HttpPost httpPost = new HttpPost(url);
         if (json != null) {
             httpPost.setEntity(new StringEntity(json, ContentType.APPLICATION_JSON));
-            log.info("doPost sending JSON="+json);
+            log.info("doPost("+url+") sending JSON=" + json);
         }
         return getResponse(client, httpPost);
     }
 
-    public RestResponse doPut(String path, String json) throws Exception {
-        HttpClient client = new DefaultHttpClient();
-        HttpPut httpPut = new HttpPut(baseUri+path);
+    protected RestResponse doPut(String path, String json) throws Exception {
+        HttpClient client = getHttpClient();
+        final String url = getUrl(path, getBaseUri());
+        @Cleanup("releaseConnection") HttpPut httpPut = new HttpPut(url);
         if (json != null) {
             httpPut.setEntity(new StringEntity(json, ContentType.APPLICATION_JSON));
             log.info("doPut sending JSON="+json);
@@ -48,16 +55,43 @@ public class WizardClient {
         return getResponse(client, httpPut);
     }
 
+    protected RestResponse doDelete(String path) throws Exception {
+        HttpClient client = getHttpClient();
+        final String url = getUrl(path, getBaseUri());
+        @Cleanup("releaseConnection") HttpDelete httpDelete = new HttpDelete(url);
+        return getResponse(client, httpDelete);
+    }
+
+    private String getUrl(String path, String clientUri) {
+        if (path.startsWith("http://") || path.startsWith("https://")) {
+            return path; // caller has supplied an absolute path
+
+        } else if (path.startsWith("/") && clientUri.endsWith("/")) {
+            path = path.substring(1); // caller has supplied a relative path
+        }
+        return clientUri + path;
+    }
+
     protected RestResponse getResponse(HttpClient client, HttpRequestBase request) throws IOException {
         final HttpResponse response = client.execute(request);
         final int statusCode = response.getStatusLine().getStatusCode();
-        String responseJson;
-        try (InputStream in = response.getEntity().getContent()) {
-            responseJson = IOUtils.toString(in);
-            log.info("read response callback server: "+responseJson);
+        final String responseJson;
+        final HttpEntity entity = response.getEntity();
+        if (entity != null) {
+            try (InputStream in = entity.getContent()) {
+                responseJson = IOUtils.toString(in);
+                log.info("read response callback server: "+responseJson);
+            }
+        } else {
+            responseJson = null;
         }
-        return new RestResponse(statusCode, responseJson);
+        return new RestResponse(statusCode, responseJson, getLocationHeader(response));
     }
 
+    public static final String LOCATION_HEADER = "Location";
+    private String getLocationHeader(HttpResponse response) {
+        final Header header = response.getFirstHeader(LOCATION_HEADER);
+        return header == null ? null : header.getValue();
+    }
 
 }
--- wizard-server-test/src/main/java/org/cobbzilla/wizardtest/resources/AbstractResourceIT.java
+++ wizard-server-test/src/main/java/org/cobbzilla/wizardtest/resources/AbstractResourceIT.java
@@ -1,18 +1,9 @@
 package org.cobbzilla.wizardtest.resources;
 
-import lombok.Cleanup;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.IOUtils;
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.*;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.cobbzilla.util.http.HttpStatusCodes;
 import org.cobbzilla.util.json.JsonUtil;
+import org.cobbzilla.wizard.client.WizardClient;
 import org.cobbzilla.wizard.exceptionmappers.ConstraintViolationBean;
 import org.cobbzilla.wizard.model.Identifiable;
 import org.cobbzilla.wizard.server.RestServer;
@@ -23,8 +14,6 @@ import org.cobbzilla.wizard.util.RestResponse;
 import org.junit.After;
 import org.junit.Before;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -33,7 +22,7 @@ import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 
 @Slf4j
-public abstract class AbstractResourceIT<C extends RestServerConfiguration, S extends RestServer<C>> {
+public abstract class AbstractResourceIT<C extends RestServerConfiguration, S extends RestServer<C>> extends WizardClient {
 
     public static final String EMPTY_JSON = "{}";
     public static final String EMPTY_JSON_ARRAY = "[]";
@@ -43,6 +32,8 @@ public abstract class AbstractResourceIT<C extends RestServerConfiguration, S ex
 
     protected static RestServerHarness<? extends RestServerConfiguration, ? extends RestServer> serverHarness = null;
     protected static RestServer server = null;
+
+    @Override public String getBaseUri() { return server.getClientUri(); }
 
     public void beforeServerStart () throws Exception {}
     public boolean shouldCacheServer () { return true; }
@@ -60,10 +51,6 @@ public abstract class AbstractResourceIT<C extends RestServerConfiguration, S ex
     }
 
     protected Map<String, String> getServerEnvironment() { return null; }
-
-    protected HttpClient getHttpClient () {
-        return new DefaultHttpClient();
-    }
 
     @After
     public void stopServer () throws Exception {
@@ -89,74 +76,6 @@ public abstract class AbstractResourceIT<C extends RestServerConfiguration, S ex
         for (String message : violationMessages) {
             assertNotNull(violations.get(message));
         }
-    }
-
-    protected RestResponse doGet(String path) throws Exception {
-        HttpClient client = getHttpClient();
-        final String url = getUrl(path, server.getClientUri());
-        @Cleanup("releaseConnection") HttpGet httpGet = new HttpGet(url);
-        return getResponse(client, httpGet);
-    }
-
-    protected RestResponse doPost(String path, String json) throws Exception {
-        HttpClient client = getHttpClient();
-        final String url = getUrl(path, server.getClientUri());
-        @Cleanup("releaseConnection") HttpPost httpPost = new HttpPost(url);
-        if (json != null) {
-            httpPost.setEntity(new StringEntity(json, ContentType.APPLICATION_JSON));
-            log.info("doPost("+url+") sending JSON=" + json);
-        }
-        return getResponse(client, httpPost);
-    }
-
-    protected RestResponse doPut(String path, String json) throws Exception {
-        HttpClient client = getHttpClient();
-        final String url = getUrl(path, server.getClientUri());
-        @Cleanup("releaseConnection") HttpPut httpPut = new HttpPut(url);
-        if (json != null) {
-            httpPut.setEntity(new StringEntity(json, ContentType.APPLICATION_JSON));
-            log.info("doPut sending JSON="+json);
-        }
-        return getResponse(client, httpPut);
-    }
-
-    protected RestResponse doDelete(String path) throws Exception {
-        HttpClient client = getHttpClient();
-        final String url = getUrl(path, server.getClientUri());
-        @Cleanup("releaseConnection") HttpDelete httpDelete = new HttpDelete(url);
-        return getResponse(client, httpDelete);
-    }
-
-    private String getUrl(String path, String clientUri) {
-        if (path.startsWith("http://") || path.startsWith("https://")) {
-            return path; // caller has supplied an absolute path
-
-        } else if (path.startsWith("/") && clientUri.endsWith("/")) {
-            path = path.substring(1); // caller has supplied a relative path
-        }
-        return clientUri + path;
-    }
-
-    protected RestResponse getResponse(HttpClient client, HttpRequestBase request) throws IOException {
-        final HttpResponse response = client.execute(request);
-        final int statusCode = response.getStatusLine().getStatusCode();
-        final String responseJson;
-        final HttpEntity entity = response.getEntity();
-        if (entity != null) {
-            try (InputStream in = entity.getContent()) {
-                responseJson = IOUtils.toString(in);
-                log.info("read response callback server: "+responseJson);
-            }
-        } else {
-            responseJson = null;
-        }
-        return new RestResponse(statusCode, responseJson, getLocationHeader(response));
-    }
-
-    public static final String LOCATION_HEADER = "Location";
-    private String getLocationHeader(HttpResponse response) {
-        final Header header = response.getFirstHeader(LOCATION_HEADER);
-        return header == null ? null : header.getValue();
     }
 
     protected <R extends Identifiable> R getEntity(Class<R> clazz, String endpoint, String uuid) throws Exception {
