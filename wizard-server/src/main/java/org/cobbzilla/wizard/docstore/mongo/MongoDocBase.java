396636371dbe15b8c66dbf6cdae12662c035355e
jonathan <jonathan@kyuss.org>
2013-09-18 12:46:27 -0700

    fix jackson package

--- wizard-server/src/main/java/org/cobbzilla/wizard/docstore/mongo/MongoDocBase.java
+++ wizard-server/src/main/java/org/cobbzilla/wizard/docstore/mongo/MongoDocBase.java
@@ -1,17 +1,15 @@
 package org.cobbzilla.wizard.docstore.mongo;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.github.jmkgreen.morphia.annotations.Id;
 import com.github.jmkgreen.morphia.annotations.Indexed;
 import lombok.Getter;
 import lombok.Setter;
 import org.bson.types.ObjectId;
-import org.cobbzilla.wizard.validation.HasValue;
-import org.codehaus.jackson.annotate.JsonIgnore;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import static org.cobbzilla.wizard.model.BasicConstraintConstants.ERR_UUID_EMPTY;
 import static org.cobbzilla.wizard.model.BasicConstraintConstants.ERR_UUID_LENGTH;
 import static org.cobbzilla.wizard.model.BasicConstraintConstants.UUID_MAXLEN;
 
