c6881e69c737ea0c2b9721fc869c2f90777aa40e
jonathan <jonathan@kyuss.org>
2014-02-12 19:40:24 -0700

    add static asset handler

---
+++ wizard-server/src/main/java/org/cobbzilla/wizard/server/StaticAssetHandler.java
@@ -0,0 +1,21 @@
+package org.cobbzilla.wizard.server;
+
+import org.glassfish.grizzly.http.server.CLStaticHttpHandler;
+import org.glassfish.grizzly.http.server.Request;
+import org.glassfish.grizzly.http.server.Response;
+
+public class StaticAssetHandler extends CLStaticHttpHandler {
+
+    public StaticAssetHandler(ClassLoader classLoader, String... docRoots) {
+        super(classLoader, docRoots);
+    }
+
+    @Override
+    protected boolean handle(String resourcePath, Request request, Response response) throws Exception {
+        if (resourcePath.equals("") || resourcePath.equals("/") || resourcePath.equals("index.php")) {
+            return super.handle("/index.html", request, response);
+        }
+        return super.handle(resourcePath, request, response);
+    }
+
+}
