a5ff04670b3d04e51c7ab92734c6e40ae41fe47e
jonathan <jonathan@kyuss.org>
2014-02-22 09:12:42 -0800

    add simple violation support for single constraint violations detected in application code

---
+++ wizard-common/src/main/java/org/cobbzilla/wizard/validation/SimpleViolationException.java
@@ -0,0 +1,16 @@
+package org.cobbzilla.wizard.validation;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+public class SimpleViolationException extends RuntimeException {
+
+    @Getter private final String messageTemplate;
+    @Getter private final String message;
+    @Getter private final String invalidValue;
+
+    public SimpleViolationException (String messageTemplate, String message) {
+        this(messageTemplate, message, null);
+    }
+}
---
+++ wizard-server/src/main/java/org/cobbzilla/wizard/exceptionmappers/SimpleViolationExceptionMapper.java
@@ -0,0 +1,30 @@
+package org.cobbzilla.wizard.exceptionmappers;
+
+import org.cobbzilla.util.http.HttpStatusCodes;
+import org.cobbzilla.wizard.validation.SimpleViolationException;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.ArrayList;
+import java.util.List;
+
+@Provider
+public class SimpleViolationExceptionMapper implements ExceptionMapper<SimpleViolationException> {
+
+    @Override
+    public Response toResponse(SimpleViolationException e) {
+        return Response.status(HttpStatusCodes.UNPROCESSABLE_ENTITY)
+                .type(MediaType.APPLICATION_JSON)
+                .entity(getEntity(e))
+                .build();
+    }
+
+    private List<ConstraintViolationBean> getEntity(SimpleViolationException e) {
+        final List<ConstraintViolationBean> jsonList = new ArrayList<>(1);
+        jsonList.add(new ConstraintViolationBean(e.getMessageTemplate(), e.getMessage(), e.getInvalidValue()));
+        return jsonList;
+    }
+
+}
