477a69c07f3f9b669429a9a178d3c01f86f7b0f1
jonathan <jonathan@kyuss.org>
2014-06-10 16:19:14 -0700

    fix name property. add @MappedSuperclass attribuet.

--- wizard-server/src/main/java/org/cobbzilla/wizard/model/UniquelyNamedEntity.java
+++ wizard-server/src/main/java/org/cobbzilla/wizard/model/UniquelyNamedEntity.java
@@ -1,11 +1,13 @@
 package org.cobbzilla.wizard.model;
 
 import lombok.EqualsAndHashCode;
+import org.cobbzilla.util.string.StringUtil;
 
 import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.Size;
 
-@EqualsAndHashCode(of={"name"}, callSuper=false)
+@MappedSuperclass @EqualsAndHashCode(of={"name"}, callSuper=false)
 public abstract class UniquelyNamedEntity<T extends IdentifiableBase> extends IdentifiableBase {
 
     protected boolean forceLowercase () { return true; }
@@ -13,8 +15,10 @@ public abstract class UniquelyNamedEntity<T extends IdentifiableBase> extends Id
     @Column(length=100, unique=true, nullable=false)
     @Size(max=100)
     protected String name;
-    public String getName () { return hasUuid() ? (forceLowercase() ? getUuid().toLowerCase() : getUuid()) : null; }
-    public void setName (String name) { this.setUuid(name == null ? null : (forceLowercase() ? name.toLowerCase() : name)); }
+    public boolean hasName () { return !StringUtil.empty(name); }
+
+    public String getName () { return hasName() ? (forceLowercase() ? name.toLowerCase() : name) : name; }
+    public void setName (String name) { this.name = (name == null ? null : forceLowercase() ? name.toLowerCase() : name); }
     public T withName (String n) { setName(n); return (T) this; }
 
 }
