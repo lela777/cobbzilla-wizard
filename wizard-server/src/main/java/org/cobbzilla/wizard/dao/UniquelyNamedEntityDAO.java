26a088986641cf29845a69f164dbf341a95abdee
jonathan <jonathan@kyuss.org>
2014-10-04 02:54:58 -0700

    abstract unique wowzaz

--- wizard-server/src/main/java/org/cobbzilla/wizard/dao/UniquelyNamedEntityDAO.java
+++ wizard-server/src/main/java/org/cobbzilla/wizard/dao/UniquelyNamedEntityDAO.java
@@ -2,7 +2,7 @@ package org.cobbzilla.wizard.dao;
 
 import org.cobbzilla.wizard.model.UniquelyNamedEntity;
 
-public class UniquelyNamedEntityDAO<E extends UniquelyNamedEntity> extends AbstractCRUDDAO<E> {
+public abstract class UniquelyNamedEntityDAO<E extends UniquelyNamedEntity> extends AbstractUniqueCRUDDAO<E> {
 
     public boolean forceLowercase () { return true; }
 
