c5f378cf6791028a8af522d4e7de1d1f7beefb65
jonathan <jonathan@kyuss.org>
2014-11-16 10:11:37 -0800

    add miredot annotations for api docs

--- wizard-server/src/main/java/org/cobbzilla/wizard/resources/AbstractSessionsResource.java
+++ wizard-server/src/main/java/org/cobbzilla/wizard/resources/AbstractSessionsResource.java
@@ -1,5 +1,6 @@
 package org.cobbzilla.wizard.resources;
 
+import com.qmino.miredot.annotations.ReturnType;
 import lombok.extern.slf4j.Slf4j;
 import org.cobbzilla.wizard.dao.AbstractSessionDAO;
 import org.cobbzilla.wizard.model.Identifiable;
@@ -15,8 +16,15 @@ public abstract class AbstractSessionsResource<T extends Identifiable> {
 
     protected abstract AbstractSessionDAO<T> getSessionDAO();
 
+    /**
+     * Lookup a session
+     * @param uuid The session ID
+     * @return The object associated with the session
+     * @statuscode 404 session not found
+     */
     @GET
     @Path("/{uuid}")
+    @ReturnType("org.cobbzilla.wizard.model.Identifiable")
     public Response find (@PathParam("uuid") String uuid) {
 
         final T entity = getSessionDAO().find(uuid);
@@ -25,22 +33,40 @@ public abstract class AbstractSessionsResource<T extends Identifiable> {
         return Response.ok(entity).build();
     }
 
+    /**
+     * Create a session
+     * @param entity The object to store for the session
+     * @return The object that was stored
+     */
     @PUT
+    @ReturnType("org.cobbzilla.wizard.model.Identifiable")
     public Response create (T entity) {
         getSessionDAO().create(entity);
         return Response.ok(entity).build();
     }
 
+    /**
+     * Update a session
+     * @param entity The new object data
+     * @return The object that was stored
+     */
     @POST
     @Path("/{uuid}")
+    @ReturnType("org.cobbzilla.wizard.model.Identifiable")
     public Response update (@PathParam("uuid") String uuid, T entity) {
         entity.setUuid(uuid);
         getSessionDAO().update(uuid, entity);
         return Response.ok(entity).build();
     }
 
+    /**
+     * Delete a session
+     * @return Just an HTTP code
+     * @statuscode 200 in all cases
+     */
     @DELETE
     @Path("/{uuid}")
+    @ReturnType("java.lang.Void")
     public Response invalidate (@PathParam("uuid") String uuid) {
         getSessionDAO().invalidate(uuid);
         return Response.ok().build();
