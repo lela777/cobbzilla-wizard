fffd34ba806ca9744deb2d4a9e916102013ee3cd
jonathan <jonathan@kyuss.org>
2015-02-27 10:01:48 -0800

    add encodeParams util

--- wizard-server/src/main/java/org/cobbzilla/wizard/util/HttpContextUtil.java
+++ wizard-server/src/main/java/org/cobbzilla/wizard/util/HttpContextUtil.java
@@ -8,14 +8,17 @@ public class HttpContextUtil {
 
     public static String getQueryParams(HttpContext context) {
 
-        final StringBuilder b = new StringBuilder();
         final MultivaluedMap<String, String> params = context.getRequest().getQueryParameters();
 
+        return "?"+ encodeParams(params);
+    }
+
+    public static String encodeParams(MultivaluedMap<String, String> params) {
+        final StringBuilder b = new StringBuilder();
         if (params != null && !params.isEmpty()) {
             for (String name : params.keySet()) {
                 for (String value : params.get(name)) {
                     if (b.length() > 0) b.append("&");
-                    if (b.length() == 0) b.append("?");
                     b.append(name).append("=").append(value);
                 }
             }
