9180159520080533a26a9571564106f252ceb477
jonathan <jonathan@kyuss.org>
2015-06-25 21:55:12 -0700

    add DnsRecordValidator

---
+++ wizard-common/src/main/java/org/cobbzilla/wizard/validation/DnsRecordValidator.java
@@ -0,0 +1,34 @@
+package org.cobbzilla.wizard.validation;
+
+import org.cobbzilla.util.dns.DnsRecord;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DnsRecordValidator {
+
+    public static List<ConstraintViolationBean> validate (DnsRecord dnsRecord) {
+
+        final List<ConstraintViolationBean> errors = new ArrayList<>();
+
+        if (!ValidationRegexes.HOST_PATTERN.matcher(dnsRecord.getFqdn()).matches()) {
+            errors.add(new ConstraintViolationBean("err.dnsRecord.fqdn.invalid"));
+        }
+
+        if (!ValidationRegexes.IPv4_PATTERN.matcher(dnsRecord.getFqdn()).matches()
+                && !ValidationRegexes.IPv6_PATTERN.matcher(dnsRecord.getFqdn()).matches()) {
+            errors.add(new ConstraintViolationBean("err.dnsRecord.ip.invalid"));
+        }
+
+        if (dnsRecord.getTtl() <= 0) {
+            errors.add(new ConstraintViolationBean("err.dnsRecord.ttl.invalid"));
+        }
+
+        if (!dnsRecord.hasAllRequiredOptions()) {
+            errors.add(new ConstraintViolationBean("err.dnsRecord.options.invalid"));
+        }
+
+        return errors;
+    }
+
+}
