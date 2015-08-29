dc8816fdd34c483dedb21dce8faf44a8804c734c
jonathan <jonathan@kyuss.org>
2015-08-29 12:00:28 -0700

    add autowire util method

--- wizard-server/src/main/java/org/cobbzilla/wizard/server/config/RestServerConfiguration.java
+++ wizard-server/src/main/java/org/cobbzilla/wizard/server/config/RestServerConfiguration.java
@@ -2,6 +2,7 @@ package org.cobbzilla.wizard.server.config;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.cobbzilla.wizard.util.SpringUtil;
 import org.springframework.context.ApplicationContext;
 
 import java.util.HashMap;
@@ -20,7 +21,9 @@ public class RestServerConfiguration {
 
     @Getter @Setter private HttpConfiguration http;
     @Getter @Setter private JerseyConfiguration jersey;
+
     @Getter @Setter private ApplicationContext applicationContext;
+    public <T> T autowire (T bean) { SpringUtil.autowire(applicationContext, bean); return bean; }
 
     @Getter @Setter private StaticHttpConfiguration staticAssets;
     public boolean hasStaticAssets () { return staticAssets != null && staticAssets.hasAssetRoot(); }
