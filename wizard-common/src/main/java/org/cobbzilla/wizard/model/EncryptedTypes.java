87f7e58c798b853f95300a43c2f28ef82f8828e3
jonathan <jonathan@kyuss.org>
2015-10-12 22:46:20 -0700

    add ENC_INT constant

--- wizard-common/src/main/java/org/cobbzilla/wizard/model/EncryptedTypes.java
+++ wizard-common/src/main/java/org/cobbzilla/wizard/model/EncryptedTypes.java
@@ -3,6 +3,7 @@ package org.cobbzilla.wizard.model;
 public class EncryptedTypes {
 
     public static final int ENC_PAD = 100;
+    public static final int ENC_INT = 100;
 
     public static final String ENCRYPTED_STRING = "encryptedString";
     public static final String STRING_ENCRYPTOR_NAME = "hibernateStringEncryptor";
