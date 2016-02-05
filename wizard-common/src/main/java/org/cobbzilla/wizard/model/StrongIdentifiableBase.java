9127f01e9ccb51eb61b14f1d6c625b725c109d09
jonathan <jonathan@kyuss.org>
2016-02-04 19:27:02 -0800

    add StrongIdentifiableBase, appends current timestamp to random uuid for VERY unique uuids

---
+++ wizard-common/src/main/java/org/cobbzilla/wizard/model/StrongIdentifiableBase.java
@@ -0,0 +1,11 @@
+package org.cobbzilla.wizard.model;
+
+import javax.persistence.MappedSuperclass;
+import java.util.UUID;
+
+@MappedSuperclass
+public class StrongIdentifiableBase extends IdentifiableBase {
+
+    @Override public void initUuid() { setUuid(UUID.randomUUID().toString() + "-" + System.currentTimeMillis()); }
+    
+}
