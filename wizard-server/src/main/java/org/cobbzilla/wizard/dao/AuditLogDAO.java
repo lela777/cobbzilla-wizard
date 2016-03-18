7776d1a28e3b485bddc78ac860abaa5a43a4bbf0
jonathan <jonathan@kyuss.org>
2016-03-18 14:42:14 -0700

    only encrypt logs if key is set

--- wizard-server/src/main/java/org/cobbzilla/wizard/dao/AuditLogDAO.java
+++ wizard-server/src/main/java/org/cobbzilla/wizard/dao/AuditLogDAO.java
@@ -4,16 +4,20 @@ import org.cobbzilla.wizard.model.AuditLog;
 
 import javax.validation.Valid;
 
+import static org.cobbzilla.util.daemon.ZillaRuntime.empty;
+
 public abstract class AuditLogDAO<E extends AuditLog> extends AbstractCRUDDAO<E> {
 
     public abstract String getEncryptionKey ();
 
     @Override public Object preCreate(@Valid E entity) {
-        return super.preCreate((E) entity.encrypt(getEncryptionKey()));
+        final String key = getEncryptionKey();
+        return empty(key) ? super.preCreate(entity) : super.preCreate((E) entity.encrypt(key));
     }
 
     @Override public Object preUpdate(@Valid E entity) {
-        return super.preUpdate((E) entity.encrypt(getEncryptionKey()));
+        final String key = getEncryptionKey();
+        return empty(key) ? super.preCreate(entity) : super.preCreate((E) entity.encrypt(key));
     }
 
 }
