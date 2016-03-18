2a77c9383533829a10327d2e756fa5d8a278d5af
jonathan <jonathan@kyuss.org>
2016-03-18 11:33:08 -0700

    add encryption to audit log

--- wizard-server/src/main/java/org/cobbzilla/wizard/dao/AuditLogDAO.java
+++ wizard-server/src/main/java/org/cobbzilla/wizard/dao/AuditLogDAO.java
@@ -2,4 +2,18 @@ package org.cobbzilla.wizard.dao;
 
 import org.cobbzilla.wizard.model.AuditLog;
 
-public class AuditLogDAO<E extends AuditLog> extends AbstractCRUDDAO<E> {}
+import javax.validation.Valid;
+
+public abstract class AuditLogDAO<E extends AuditLog> extends AbstractCRUDDAO<E> {
+
+    public abstract String getEncryptionKey ();
+
+    @Override public Object preCreate(@Valid E entity) {
+        return super.preCreate((E) entity.encrypt(getEncryptionKey()));
+    }
+
+    @Override public Object preUpdate(@Valid E entity) {
+        return super.preUpdate((E) entity.encrypt(getEncryptionKey()));
+    }
+
+}
