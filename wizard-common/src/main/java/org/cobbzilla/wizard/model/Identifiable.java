c32430b89bebee7c782f24748f3609b89399b35f
jonathan <jonathan@kyuss.org>
2016-03-28 12:47:00 -0700

    add UUID_ARRAY constant

--- wizard-common/src/main/java/org/cobbzilla/wizard/model/Identifiable.java
+++ wizard-common/src/main/java/org/cobbzilla/wizard/model/Identifiable.java
@@ -2,6 +2,8 @@ package org.cobbzilla.wizard.model;
 
 public interface Identifiable {
 
+    String[] UUID_ARRAY = {"uuid"};
+
     int UUID_MAXLEN = BasicConstraintConstants.UUID_MAXLEN;
 
     String getUuid();
