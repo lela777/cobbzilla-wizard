cb1a79852fff4bba6f850d34db26fe5df9eb9e03
jonathan <jonathan@kyuss.org>
2016-04-03 23:58:47 -0700

    allow server urls to be empty

--- wizard-server/src/main/java/org/cobbzilla/wizard/server/config/ElasticSearchConfig.java
+++ wizard-server/src/main/java/org/cobbzilla/wizard/server/config/ElasticSearchConfig.java
@@ -2,10 +2,29 @@ package org.cobbzilla.wizard.server.config;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.cobbzilla.util.collection.SingletonList;
+
+import java.util.Iterator;
+import java.util.List;
+
+import static org.cobbzilla.util.daemon.ZillaRuntime.empty;
 
 public class ElasticSearchConfig {
 
+    public static final String DEFAULT_ES_URL = "http://127.0.0.1:9200";
+    public static final SingletonList<String> DEFAULT_ES_LIST = new SingletonList<>(DEFAULT_ES_URL);
+
     @Getter @Setter private String cluster = "elasticsearch";
-    @Getter @Setter private String[] servers;
+    @Setter private List<String> servers;
+
+    public List<String> getServers () {
+        if (empty(servers)) return DEFAULT_ES_LIST;
+        for (Iterator<String> iter = servers.iterator(); iter.hasNext(); ) {
+            // it's ok if a name is empty, we just skip it
+            final String server = iter.next();
+            if (empty(server)) iter.remove();
+        }
+        return servers;
+    }
 
 }
