59c0eb48a27f0dab8b2d02e25873e52f76e63033
jonathan <jonathan@kyuss.org>
2016-04-11 19:53:51 -0700

    improve support for boolean/flag fields

--- wizard-common/src/main/java/org/cobbzilla/wizard/model/entityconfig/EntityFieldConfig.java
+++ wizard-common/src/main/java/org/cobbzilla/wizard/model/entityconfig/EntityFieldConfig.java
@@ -3,18 +3,19 @@ package org.cobbzilla.wizard.model.entityconfig;
 import lombok.Getter;
 import lombok.Setter;
 
-import static org.cobbzilla.util.daemon.ZillaRuntime.empty;
-
 public class EntityFieldConfig {
 
-    @Getter @Setter private String name;
-    @Setter private String displayName;
-
-    public String getDisplayName() { return !empty(displayName) ? displayName : getName(); }
-
+    @Getter @Setter private String displayName;
     @Getter @Setter private EntityFieldMode mode = EntityFieldMode.standard;
     @Getter @Setter private EntityFieldType type = EntityFieldType.string;
-    @Getter @Setter private EntityFieldControl control = EntityFieldControl.text;
+
+    @Setter private EntityFieldControl control;
+    public EntityFieldControl getControl() {
+        if (control != null) return control;
+        if (type == EntityFieldType.flag) return EntityFieldControl.checkbox;
+        return EntityFieldControl.text;
+    }
+
     @Getter @Setter private EntityFieldReference reference = null;
     @Getter @Setter private Integer length = null;
 
--- wizard-common/src/main/java/org/cobbzilla/wizard/model/entityconfig/EntityFieldType.java
+++ wizard-common/src/main/java/org/cobbzilla/wizard/model/entityconfig/EntityFieldType.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 
 public enum EntityFieldType {
 
-    string, integer, decimal, epoch_time, reference;
+    string, integer, decimal, flag, epoch_time, reference;
 
     @JsonCreator public static EntityFieldType create (String val) { return valueOf(val.toLowerCase()); }
 }
