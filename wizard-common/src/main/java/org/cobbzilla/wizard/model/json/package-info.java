9cf90739eab38e10406d0beb401077cd30868cc0
jonathan <jonathan@kyuss.org>
2016-04-24 11:32:32 -0700

    add support for jsonb data types

--- wizard-common/src/main/java/org/cobbzilla/wizard/model/json/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * see: https://stackoverflow.com/a/16049086/1251543
- */
-@TypeDefs( {@TypeDef( name= StringJsonUserType.JSONB_TYPE, typeClass = StringJsonUserType.class)})
-
-package org.cobbzilla.wizard.model.json;
-
-import org.hibernate.annotations.TypeDef;
-import org.hibernate.annotations.TypeDefs;
