d2069b7b318648873a6c46ab6eb24074a565c0fd
jonathan <jonathan@kyuss.org>
2016-05-02 13:04:02 -0700

    improve JSON marshalling for options lists

--- wizard-common/src/main/java/org/cobbzilla/wizard/model/entityconfig/EntityFieldConfig.java
+++ wizard-common/src/main/java/org/cobbzilla/wizard/model/entityconfig/EntityFieldConfig.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.cobbzilla.util.daemon.ZillaRuntime.die;
 import static org.cobbzilla.util.daemon.ZillaRuntime.empty;
 import static org.cobbzilla.util.json.JsonUtil.json;
 import static org.cobbzilla.util.string.StringUtil.camelCaseToString;
@@ -30,21 +31,36 @@ public class EntityFieldConfig {
         return EntityFieldControl.text;
     }
 
+    // value of 'options' may be:
+    //  * a comma-separated string of values
+    //  * JSON representing an array of EntityFieldOption objects
+    //  * a special string 'uri:api-path:value:displayValue'
+    //      this means:
+    //        - do a GET of api-path
+    //        - expect the response to be a JSON array of objects
+    //        - for each object in the array, use the 'value' field for the option value, and the 'displayValue' field for the option's display value
     @Getter @Setter private String options;
+
+    // this is a special option that indicates no selection has been made
     @Getter @Setter private String emptyDisplayValue;
 
-    @JsonIgnore public EntityFieldOption[] getOptionsList() {
+    // Not exposed in JSON, since 'options' should be represented as a String when writing JSON
+    @JsonIgnore public EntityFieldOption[] getOptionsArray() {
+
         if (empty(options)) return EMPTY_OPTIONS_ARRAY;
+        if (options.startsWith("uri:"))  die("getOptionsArray: cannot convert uri-style options to array: "+options);
+
         if (options.trim().startsWith("[")) {
             return json(options, EntityFieldOption[].class);
         } else {
             final List<EntityFieldOption> opts = new ArrayList<>();
-            for (String opt : options.split(",")) {
-                opts.add(new EntityFieldOption(opt));
-            }
+            for (String opt : options.split(",")) opts.add(new EntityFieldOption(opt.trim()));
             return opts.toArray(new EntityFieldOption[opts.size()]);
         }
     }
+
+    // We do allow inbound-JSON to set options using a JSON array
+    // Much nicer than nesting JSON-in-JSON, which would otherwise be required
     public void setOptionsList(EntityFieldOption[] options) { this.options = json(options); }
 
     @Getter @Setter private EntityFieldReference reference = null;
