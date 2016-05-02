37e7ad6c5c706c3f1491bac5e79dc14ce6161b3b
jonathan <jonathan@kyuss.org>
2016-05-02 12:35:45 -0700

    add EntityFieldConfig.getOptionsList

--- wizard-common/src/main/java/org/cobbzilla/wizard/model/entityconfig/EntityFieldConfig.java
+++ wizard-common/src/main/java/org/cobbzilla/wizard/model/entityconfig/EntityFieldConfig.java
@@ -4,12 +4,16 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.cobbzilla.util.daemon.ZillaRuntime.empty;
 import static org.cobbzilla.util.json.JsonUtil.json;
 import static org.cobbzilla.util.string.StringUtil.camelCaseToString;
 
 public class EntityFieldConfig {
 
+    public static final EntityFieldOption[] EMPTY_OPTIONS_ARRAY = new EntityFieldOption[0];
     @Getter @Setter private String name;
 
     @Setter private String displayName;
@@ -29,6 +33,18 @@ public class EntityFieldConfig {
     @Getter @Setter private String options;
     @Getter @Setter private String emptyDisplayValue;
 
+    @JsonIgnore public EntityFieldOption[] getOptionsList() {
+        if (empty(options)) return EMPTY_OPTIONS_ARRAY;
+        if (options.trim().startsWith("[")) {
+            return json(options, EntityFieldOption[].class);
+        } else {
+            final List<EntityFieldOption> opts = new ArrayList<>();
+            for (String opt : options.split(",")) {
+                opts.add(new EntityFieldOption(opt));
+            }
+            return opts.toArray(new EntityFieldOption[opts.size()]);
+        }
+    }
     public void setOptionsList(EntityFieldOption[] options) { this.options = json(options); }
 
     @Getter @Setter private EntityFieldReference reference = null;
