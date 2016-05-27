171162c29b8b343b2c525da8e6f641b4d70b9606
jonathan <jonathan@kyuss.org>
2016-05-27 12:35:36 -0700

    add more robust support for offset from current time

--- wizard-common/src/main/java/org/cobbzilla/wizard/validation/FutureDateValidator.java
+++ wizard-common/src/main/java/org/cobbzilla/wizard/validation/FutureDateValidator.java
@@ -4,6 +4,7 @@ import org.joda.time.format.DateTimeFormat;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.util.concurrent.TimeUnit;
 
 import static org.cobbzilla.util.daemon.ZillaRuntime.empty;
 import static org.cobbzilla.util.daemon.ZillaRuntime.now;
@@ -15,10 +16,24 @@ public class FutureDateValidator implements ConstraintValidator<FutureDate, Obje
     private boolean emptyOk;
 
     @Override public void initialize(FutureDate constraintAnnotation) {
-        this.min = constraintAnnotation.min();
+        this.min = parseMin(constraintAnnotation.min());
         this.format = constraintAnnotation.format();
         this.emptyOk = constraintAnnotation.emptyOk();
     }
+
+    private long parseMin(String min) {
+        if (empty(min)) return 0;
+        final long val = Long.parseLong(chopSuffix(min));
+        switch (min.charAt(min.length()-1)) {
+            case 's': return TimeUnit.SECONDS.toMillis(val);
+            case 'm': return TimeUnit.MINUTES.toMillis(val);
+            case 'h': return TimeUnit.HOURS.toMillis(val);
+            case 'd': return TimeUnit.DAYS.toMillis(val);
+            default: return Long.parseLong(min);
+        }
+    }
+
+    private String chopSuffix(String min) { return min.substring(0, min.length()-1); }
 
     @Override public boolean isValid(Object value, ConstraintValidatorContext context) {
         if (empty(value)) return emptyOk;
