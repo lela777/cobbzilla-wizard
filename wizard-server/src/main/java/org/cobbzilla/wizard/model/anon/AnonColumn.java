fcf69be2cc7b4847245b840f68df4c584edd85eb
jonathan <jonathan@kyuss.org>
2016-08-23 04:57:41 -0700

    add final modifier

--- wizard-server/src/main/java/org/cobbzilla/wizard/model/anon/AnonColumn.java
+++ wizard-server/src/main/java/org/cobbzilla/wizard/model/anon/AnonColumn.java
@@ -53,7 +53,7 @@ public class AnonColumn {
     }
 
     private String transformJson(String value) throws Exception {
-        ObjectNode node = JsonUtil.json(value, ObjectNode.class);
+        final ObjectNode node = JsonUtil.json(value, ObjectNode.class);
         for (AnonJsonPath jsonPath : json) {
             final JsonNode toReplace = findNode(node, jsonPath.getPath());
             if (toReplace != null) {
