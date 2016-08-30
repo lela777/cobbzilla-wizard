6167b327ed65bf59151bd859054c45076525dd04
jonathan <jonathan@kyuss.org>
2016-08-29 17:12:17 -0700

    allow multiple related entities of the same type

--- wizard-common/src/main/java/org/cobbzilla/wizard/model/SqlViewField.java
+++ wizard-common/src/main/java/org/cobbzilla/wizard/model/SqlViewField.java
@@ -18,23 +18,32 @@ public class SqlViewField {
     @Getter @Setter private SqlViewFieldSetter setter;
     public boolean hasSetter () { return setter != null; }
 
-    public SqlViewField(String name)                    { this(null, name, snakeCaseToCamelCase(name), false, null); }
-    public SqlViewField(String name, String property)   { this(null, name, property, false, null); }
-    public SqlViewField(String name, SqlViewFieldSetter setter)   { this(null, name, snakeCaseToCamelCase(name), false, setter); }
-    public SqlViewField(String name, boolean encrypted) { this(null, name, snakeCaseToCamelCase(name), encrypted, null); }
-    public SqlViewField(String name, String property, boolean encrypted) { this(null, name, property, encrypted, null); }
+    public SqlViewField(String name)                    { this(null, name, snakeCaseToCamelCase(name), false, null, null); }
+    public SqlViewField(String name, String property)   { this(null, name, property, false, null, null); }
+    public SqlViewField(String name, SqlViewFieldSetter setter)   { this(null, name, snakeCaseToCamelCase(name), false, setter, null); }
+    public SqlViewField(String name, boolean encrypted) { this(null, name, snakeCaseToCamelCase(name), encrypted, null, null); }
+    public SqlViewField(String name, String property, boolean encrypted) { this(null, name, property, encrypted, null, null); }
 
     public SqlViewField(Class<? extends Identifiable> type, String name, String property) {
-        this(type, name, property, false, null);
+        this(type, name, property, false, null, null);
+    }
+    public SqlViewField(Class<? extends Identifiable> type, String name, String property, String entity) {
+        this(type, name, property, false, null, entity);
     }
     public SqlViewField(Class<? extends Identifiable> type, String name, boolean encrypted) {
-        this(type, name, snakeCaseToCamelCase(name), encrypted, null);
+        this(type, name, snakeCaseToCamelCase(name), encrypted, null, null);
     }
     public SqlViewField(Class<? extends Identifiable> type, String name, String property, boolean encrypted) {
-        this(type, name, property, encrypted, null);
+        this(type, name, property, encrypted, null, null);
+    }
+    public SqlViewField(Class<? extends Identifiable> type, String name, String property, boolean encrypted, String entity) {
+        this(type, name, property, encrypted, null, entity);
     }
 
+    private String entity;
+
     public String getEntity () {
+        if (entity != null) return entity;
         if (type == null) return null;
         final int dotPos = property.indexOf('.');
         return dotPos == -1 ? null : property.substring(0, dotPos);
