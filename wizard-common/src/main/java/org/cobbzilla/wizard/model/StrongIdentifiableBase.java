8bc50656cb0329f268a936ec5ac3468cf2eed27e
jonathan <jonathan@kyuss.org>
2016-09-01 15:07:30 -0700

    code cleanup

--- wizard-common/src/main/java/org/cobbzilla/wizard/model/StrongIdentifiableBase.java
+++ wizard-common/src/main/java/org/cobbzilla/wizard/model/StrongIdentifiableBase.java
@@ -1,8 +1,9 @@
 package org.cobbzilla.wizard.model;
 
 import javax.persistence.MappedSuperclass;
-import java.util.UUID;
 
+import static java.lang.Long.toHexString;
+import static java.util.UUID.randomUUID;
 import static org.cobbzilla.util.daemon.ZillaRuntime.empty;
 import static org.cobbzilla.util.daemon.ZillaRuntime.now;
 
@@ -11,10 +12,11 @@ public class StrongIdentifiableBase extends IdentifiableBase {
 
     @Override public void initUuid() { setUuid(newStrongUuid()); }
 
-    public static String newStrongUuid() { return UUID.randomUUID().toString() + "-" + Long.toHexString(now()); }
+    public static String newStrongUuid() { return randomUUID().toString() + "-" + toHexString(now()); }
 
     public static boolean isStrongUuid(String maybeUuid) {
-        // must be non-empty, at least 40 chars, and cannot contain any letter chars that
+        // must be non-empty and contain at least 40 hex chars
+        // todo: fix this check
         return !empty(maybeUuid)
                 && maybeUuid.length() >= 40
                 && maybeUuid.replaceAll("[\\WA-Fa-f\\d]+", "").length() == 0;
