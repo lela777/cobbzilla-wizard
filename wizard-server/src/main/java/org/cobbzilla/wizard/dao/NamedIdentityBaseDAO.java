096c46ddb5323cd36040c77025616db1374a9caa
jonathan <jonathan@kyuss.org>
2016-10-06 15:10:45 -0700

    add findByNameIn methods

--- wizard-server/src/main/java/org/cobbzilla/wizard/dao/NamedIdentityBaseDAO.java
+++ wizard-server/src/main/java/org/cobbzilla/wizard/dao/NamedIdentityBaseDAO.java
@@ -2,9 +2,14 @@ package org.cobbzilla.wizard.dao;
 
 import org.cobbzilla.wizard.model.NamedIdentityBase;
 
+import java.util.List;
+
 public class NamedIdentityBaseDAO<E extends NamedIdentityBase> extends AbstractCRUDDAO<E> {
 
     public E findByName (String name) { return findByUniqueField("name", name); }
     public E findByUuid (String name) { return findByName(name); }
+
+    public List<E> findByNameIn(List<String> names) { return findByFieldIn("name", names); }
+    public List<E> findByNameIn(String[] names) { return findByFieldIn("name", names); }
 
 }
