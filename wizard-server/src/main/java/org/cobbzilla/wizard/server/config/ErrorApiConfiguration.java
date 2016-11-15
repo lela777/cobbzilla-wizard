b03f80f0de8dbd2519748ab9f866b457e379248c
jonathan <jonathan@kyuss.org>
2016-11-15 06:19:26 -0800

    use a ring buffer to avoid reporting duplicate exceptions/messages

--- wizard-server/src/main/java/org/cobbzilla/wizard/server/config/ErrorApiConfiguration.java
+++ wizard-server/src/main/java/org/cobbzilla/wizard/server/config/ErrorApiConfiguration.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.buffer.CircularFifoBuffer;
 import org.cobbzilla.util.system.CommandShell;
 
 import static org.cobbzilla.util.daemon.ZillaRuntime.empty;
@@ -25,16 +26,31 @@ public class ErrorApiConfiguration {
 
     public boolean isValid() { return !empty(getUrl()) && !empty(getKey()) && !empty(getEnv()); }
 
+    private final CircularFifoBuffer cache = new CircularFifoBuffer(20);
+
     public void report(Exception e) {
+        if (inCache(e)) return;
         final AirbrakeNoticeBuilder builder = new AirbrakeNoticeBuilder(getKey(), e, getEnv());
         final int responseCode = getNotifier().notify(builder.newNotice());
         if (responseCode != 200) log.warn("report("+e+"): notifier API returned "+responseCode);
     }
 
     public void report(String s) {
+        if (inCache(s)) return;
         final AirbrakeNoticeBuilder builder = new AirbrakeNoticeBuilder(getKey(), s, getEnv());
         final int responseCode = getNotifier().notify(builder.newNotice());
         if (responseCode != 200) log.warn("report("+s+"): notifier API returned "+responseCode);
+    }
+
+    public boolean inCache(Object o) {
+        synchronized (cache) {
+            if (cache.contains(o)) {
+                log.warn("inCache(" + o + "): already reported recently");
+                return true;
+            }
+            cache.add(o);
+        }
+        return false;
     }
 
 }
