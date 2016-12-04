9042e926c8b7c855ca57f23cdc4c13ab8fcf13c6
jonathan <jonathan@kyuss.org>
2016-12-04 10:08:25 -0800

    add logging

--- wizard-server-test/src/main/java/org/cobbzilla/wizardtest/resources/AbstractResourceIT.java
+++ wizard-server-test/src/main/java/org/cobbzilla/wizardtest/resources/AbstractResourceIT.java
@@ -131,6 +131,7 @@ public abstract class AbstractResourceIT<C extends RestServerConfiguration, S ex
                 die("beforeStart: error dropping/creating database: " + dbName);
             }
             database.setUrl(url.substring(0, lastSlash) + "/" + dbName);
+            log.info("beforeStart: using database "+dbName);
         }
     }
 
@@ -152,6 +153,8 @@ public abstract class AbstractResourceIT<C extends RestServerConfiguration, S ex
             final C configuration = server.getConfiguration();
             if (configuration instanceof HasDatabaseConfiguration) {
                 final DatabaseConfiguration database = ((HasDatabaseConfiguration) configuration).getDatabase();
+                final String dbName = database.getDatabaseName();
+                log.info("onStop: stopping connection pool for database "+dbName);
                 if (database.getPool().isEnabled()) {
                     final DataSource ds = configuration.getBean(RdbmsConfig.class).dataSource();
                     if (ds instanceof PooledDataSource) {
@@ -162,7 +165,7 @@ public abstract class AbstractResourceIT<C extends RestServerConfiguration, S ex
                         }
                     }
                 }
-                daemon(new DbDropper(database.getDatabaseName()));
+                daemon(new DbDropper(dbName));
             }
         }
     }
