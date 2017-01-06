c302fde5a6d2720e8d3be6b6c1876d270c9e0e33
jonathan <jonathan@kyuss.org>
2017-01-06 00:12:22 -0800

    add caching for NamedIdentityBase entities

--- wizard-server/src/main/java/org/cobbzilla/wizard/dao/NamedIdentityBaseDAO.java
+++ wizard-server/src/main/java/org/cobbzilla/wizard/dao/NamedIdentityBaseDAO.java
@@ -2,14 +2,55 @@ package org.cobbzilla.wizard.dao;
 
 import org.cobbzilla.wizard.model.NamedIdentityBase;
 
+import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class NamedIdentityBaseDAO<E extends NamedIdentityBase> extends AbstractCRUDDAO<E> {
 
-    public E findByName (String name) { return findByUniqueField("name", name); }
+    private final Map<String, E> cache = new ConcurrentHashMap<>();
+
+    @Override public Object preCreate(@Valid E entity) {
+        cache.remove(entity.getName());
+        return super.preCreate(entity);
+    }
+
+    @Override public Object preUpdate(@Valid E entity) {
+        cache.remove(entity.getName());
+        return super.preUpdate(entity);
+    }
+
+    public E findByName (String name) {
+        E thing = cache.get(name);
+        if (thing == null) {
+            thing = findByUniqueField("name", name);
+            cache.put(name, thing);
+        }
+        return thing;
+    }
+
     public E findByUuid (String name) { return findByName(name); }
 
-    public List<E> findByNameIn(List<String> names) { return findByFieldIn("name", names); }
+    public List<E> findByNameIn(List<String> names) {
+        final List<E> found = new ArrayList<>();
+        final List<String> notFoundNames = new ArrayList<>();
+         for (String name : names) {
+            if (cache.containsKey(name)) {
+                found.add(cache.get(name));
+            } else {
+                notFoundNames.add(name);
+            }
+        }
+        if (found.size() != names.size()) {
+            final List<E> lookedUp = findByFieldIn("name", notFoundNames);
+            for (E toCache : lookedUp) cache.put(toCache.getName(), toCache);
+            found.addAll(lookedUp);
+        }
+        return found;
+    }
+
     public List<E> findByNameIn(String[] names) { return findByFieldIn("name", names); }
 
 }
