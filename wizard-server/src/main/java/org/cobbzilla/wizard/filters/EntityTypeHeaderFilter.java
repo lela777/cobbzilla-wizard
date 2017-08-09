256c0b42c08681c45883427697e76563ab58d474
Jonathan Cobb <jonathan@kyuss.org>
2017-08-09 00:53:32 -0700

    remove special handling for Map

--- wizard-server/src/main/java/org/cobbzilla/wizard/filters/EntityTypeHeaderFilter.java
+++ wizard-server/src/main/java/org/cobbzilla/wizard/filters/EntityTypeHeaderFilter.java
@@ -8,7 +8,6 @@ import org.cobbzilla.wizard.model.Identifiable;
 
 import java.lang.reflect.Type;
 import java.util.Collection;
-import java.util.Map;
 
 import static org.cobbzilla.util.daemon.ZillaRuntime.empty;
 import static org.cobbzilla.util.reflect.ReflectionUtil.forName;
@@ -34,7 +33,6 @@ public class EntityTypeHeaderFilter implements ContainerResponseFilter {
         }
 
         boolean isCollection = Collection.class.isAssignableFrom(responseClass);
-        boolean isMap = Map.class.isAssignableFrom(responseClass);
         boolean isArray = responseClass.isArray();
         final String elementClassName;
         if (isCollection) {
@@ -47,15 +45,6 @@ public class EntityTypeHeaderFilter implements ContainerResponseFilter {
             elementClassName = empty(a) ? "" : a[0].getClass().getName();
             containerResponse.getHttpHeaders().add(getTypeHeaderName(), elementClassName + "[]");
 
-        } else if (isMap) {
-            final Map m = (Map) containerResponse.getEntity();
-            if (empty(m)) {
-                elementClassName = "";
-            } else {
-                final Map.Entry entry = (Map.Entry) m.entrySet().iterator().next();
-                elementClassName = entry.getKey().getClass() + "->" + entry.getValue().getClass();
-            }
-            containerResponse.getHttpHeaders().add(getTypeHeaderName(), Map.class.getName());
         } else {
             containerResponse.getHttpHeaders().add(getTypeHeaderName(), responseClassName);
         }
