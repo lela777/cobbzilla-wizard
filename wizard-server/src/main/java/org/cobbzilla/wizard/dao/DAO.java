c878a923ee3e86aa7f1abf45478e05bd56b9a413
Jonathan Cobb <jonathan@kyuss.org>
2017-08-18 05:52:35 -0700

    support new delete throughout DAO hierarchy

--- wizard-server/src/main/java/org/cobbzilla/wizard/dao/DAO.java
+++ wizard-server/src/main/java/org/cobbzilla/wizard/dao/DAO.java
@@ -1,5 +1,6 @@
 package org.cobbzilla.wizard.dao;
 
+import org.cobbzilla.wizard.model.Identifiable;
 import org.cobbzilla.wizard.model.ResultPage;
 
 import javax.validation.Valid;
@@ -7,7 +8,7 @@ import java.io.Serializable;
 import java.util.Collection;
 import java.util.List;
 
-public interface DAO<E> {
+public interface DAO<E extends Identifiable> {
 
     String SQL_QUERY = "SQL:";
 
