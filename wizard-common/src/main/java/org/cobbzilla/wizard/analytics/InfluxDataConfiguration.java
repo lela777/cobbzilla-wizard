3608638ea01aa7580e6dd0fd2cbf30d604ecfd38
Jelisaveta <jdragojevic@itekako.com>
2017-09-11 16:27:35 +0200

    Add metrics configuration

---
+++ wizard-common/src/main/java/org/cobbzilla/wizard/analytics/InfluxDataConfiguration.java
@@ -0,0 +1,21 @@
+package org.cobbzilla.wizard.analytics;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor @AllArgsConstructor
+public class InfluxDataConfiguration {
+
+    @Getter @Setter private String host;
+    @Getter @Setter private String port;
+    @Getter @Setter private String databaseName;
+    @Getter @Setter private String username;
+    @Getter @Setter private String password;
+
+    public String getServerUrl() { return host + ":" + port; }
+    public String getWriteUrl() {
+        return getServerUrl() + "/write?db=" + databaseName + "&u=" + username + "&p=" + password;
+    }
+}
