a6a7ebb1b3757c0f6aa3f9322c8a39fb5174eaf7
Jovan Martinovic <jmartinovic@itekako.com>
2017-09-20 13:26:01 +0200

    moved list generation to ratelimitfilter

--- wizard-server/src/main/java/org/cobbzilla/wizard/filters/RateLimitFilter.java
+++ wizard-server/src/main/java/org/cobbzilla/wizard/filters/RateLimitFilter.java
@@ -1,5 +1,7 @@
 package org.cobbzilla.wizard.filters;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.sun.jersey.spi.container.ContainerRequest;
 import com.sun.jersey.spi.container.ContainerRequestFilter;
 import lombok.Getter;
@@ -7,6 +9,7 @@ import lombok.NoArgsConstructor;
 import org.cobbzilla.util.collection.SingletonList;
 import org.cobbzilla.util.daemon.ZillaRuntime;
 import org.cobbzilla.util.io.StreamUtil;
+import org.cobbzilla.util.json.JsonUtil;
 import org.cobbzilla.wizard.cache.redis.RedisService;
 import org.springframework.stereotype.Service;
 
@@ -14,21 +17,33 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
+import java.util.ArrayList;
 import java.util.List;
 
 
 @Provider @Service @NoArgsConstructor
 public abstract class RateLimitFilter implements ContainerRequestFilter {
 
-    @Getter(lazy=true) private final RedisService buckets = initCache();
+    @Getter(lazy = true) private final RedisService buckets = initCache();
     protected abstract RedisService initCache();
 
-    @Getter(lazy=true) private final String scriptSha = initScript();
+    @Getter(lazy = true) private final String scriptSha = initScript();
     public String initScript() {
         return getBuckets().loadScript(StreamUtil.loadResourceAsStringOrDie("api/api_limiter_redis.lua"));
     }
 
-    protected abstract  List<String> getList();
+    @Getter(lazy = true) private final List<String> list = initList();
+    protected List<String> initList() {
+        ArrayNode array = JsonUtil.fromJsonOrDie(StreamUtil.loadResourceAsStringOrDie("api/api_limits.json"),
+                                                 ArrayNode.class);
+        List<String> list = new ArrayList<>();
+        for (JsonNode row : array) {
+            list.add(row.get("limit").textValue());
+            list.add(row.get("interval").textValue());
+            list.add(row.get("block_dur").textValue());
+        }
+        return list;
+    }
 
     protected abstract String getKey(ContainerRequest request);
 
@@ -41,6 +56,6 @@ public abstract class RateLimitFilter implements ContainerRequestFilter {
     }
 
     public Long checkOverflow(ContainerRequest request) {
-        return (Long)getBuckets().eval(getScriptSha(), new SingletonList<>(getKey(request)), getList());
+        return (Long) getBuckets().eval(getScriptSha(), new SingletonList<>(getKey(request)), getList());
     }
 }
