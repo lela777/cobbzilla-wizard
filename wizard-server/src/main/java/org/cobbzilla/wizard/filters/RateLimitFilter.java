2adc5d09d3def5da0ee1f009ee5881724f21aea0
Jovan Martinovic <jmartinovic@itekako.com>
2017-09-25 13:50:43 +0200

    added comments to lua script, switch limits to ApiRateLimit list, renamed buckets to cache

--- wizard-server/src/main/java/org/cobbzilla/wizard/filters/RateLimitFilter.java
+++ wizard-server/src/main/java/org/cobbzilla/wizard/filters/RateLimitFilter.java
@@ -5,10 +5,8 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.sun.jersey.spi.container.ContainerRequest;
 import com.sun.jersey.spi.container.ContainerRequestFilter;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.cobbzilla.util.collection.SingletonList;
 import org.cobbzilla.util.io.StreamUtil;
 import org.cobbzilla.wizard.cache.redis.RedisService;
@@ -18,21 +16,23 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @Provider @Service @NoArgsConstructor
 public abstract class RateLimitFilter implements ContainerRequestFilter {
 
-    @Getter(lazy = true) private final RedisService buckets = initCache();
+    @Getter(lazy = true) private final RedisService cache = initCache();
     protected abstract RedisService initCache();
 
     @Getter(lazy = true) private final String scriptSha = initScript();
     public String initScript() {
-        return getBuckets().loadScript(StreamUtil.stream2string("org/cobbzilla/wizard/filters/api_limiter_redis.lua"));
+        return getCache().loadScript(StreamUtil.stream2string("org/cobbzilla/wizard/filters/api_limiter_redis.lua"));
     }
 
-    protected final static LoadingCache<String, List<String>> keys =
+    @Getter private final static LoadingCache<String, List<String>> keys =
             CacheBuilder.newBuilder()
                         .maximumSize(1000)
                         .expireAfterAccess(5, TimeUnit.MINUTES)
@@ -45,7 +45,7 @@ public abstract class RateLimitFilter implements ContainerRequestFilter {
 
     protected abstract List<String> getKey(ContainerRequest request);
 
-    protected abstract List<String> getLimits();
+    protected abstract List<ApiRateLimit> getLimits();
 
     @Override public ContainerRequest filter(@Context ContainerRequest request) {
         Long i = checkOverflow(request);
@@ -57,12 +57,9 @@ public abstract class RateLimitFilter implements ContainerRequestFilter {
     }
 
     public Long checkOverflow(ContainerRequest request) {
-        return (Long) getBuckets().eval(getScriptSha(), getKey(request), getLimits());
-    }
-    @NoArgsConstructor @AllArgsConstructor
-    protected static class ApiRateLimit {
-        @Getter @Setter int limit;
-        @Getter @Setter long interval;
-        @Getter @Setter long block;
+        return (Long) getCache().eval(getScriptSha(), getKey(request), getLimits().stream().map(
+                x->new String[]{String.valueOf(x.limit), String.valueOf(x.interval), String.valueOf(x.block)})
+                                                                                  .flatMap(Arrays::stream)
+                                                                                  .collect(Collectors.toList()));
     }
 }
--- wizard-server/src/main/resources/org/cobbzilla/wizard/filters/api_limiter_redis.lua
+++ wizard-server/src/main/resources/org/cobbzilla/wizard/filters/api_limiter_redis.lua
@@ -1,19 +1,20 @@
-local limits = ARGV
-local len = #ARGV
-local ret = false
-for i=1,len,3 do
-    local fullkey = KEYS[1] .. ':' .. limits[i]
-    local bucket = redis.call('GET', fullkey)
-    if ( (bucket ~= false) and ( tonumber(bucket) > tonumber(limits[i])) ) then
-        ret = i
+local len = #ARGV  --ARGV is the argument array containing triples (limit, interval, block), # returns length
+local ret = false --lua false translates to redis null
+for i=1,len,3 do    --iterate over arguments from 1 to len, step 3
+    local fullkey = KEYS[1] .. ':' .. ARGV[i] .. ':' .. ARGV[i+1] .. ':' .. ARGV[i+2]
+    --KEYS is the array of keys, .. concatenates strings
+    local bucket = redis.call('GET', fullkey)   -- return false if not found
+    if ( (bucket ~= false) and ( tonumber(bucket) >= tonumber(ARGV[i])) ) then
+        ret = i  -- set last limit breach
     else
-        local count = redis.call('INCR', fullkey)
-        if tonumber(count) > tonumber(limits[i]) then
-            redis.call('PEXPIRE', fullkey, tonumber(limits[i+2]))
-            ret = i
+        local count = redis.call('INCR', fullkey) --increments the key, sets it to 1 if it does not exist
+        if tonumber(count) >= tonumber(ARGV[i]) then
+            redis.call('PEXPIRE', fullkey, tonumber(ARGV[i+2]))
+            ret = i --if the limit is breached, update the key to expire in block miliseconds, and set last limit breach
         else
             if count == 1 then
               redis.call('PEXPIRE', fullkey, tonumber(ARGV[i+1]))
+                -- if just created the key, set expiry in interval miliseconds
             end
         end
     end
