d7389ace50642ac8ad2dfb40d169be1f8fee9d39
Jonathan Cobb <jonathan@kyuss.org>
2017-09-24 20:01:24 -0700

    reuse memory when returning same log lines

--- wizard-server/src/main/java/org/cobbzilla/wizard/log/LogRelayMemoryTarget.java
+++ wizard-server/src/main/java/org/cobbzilla/wizard/log/LogRelayMemoryTarget.java
@@ -6,11 +6,14 @@ import org.cobbzilla.wizard.server.config.RestServerConfiguration;
 import javax.annotation.PostConstruct;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 public abstract class LogRelayMemoryTarget implements LogRelayAppenderTarget {
 
     private String[] lines;
     private final AtomicInteger index = new AtomicInteger(0);
+    private final AtomicInteger indexAtLastGetLines = new AtomicInteger(-1);
+    private final AtomicReference<String[]> lastLines = new AtomicReference<>(null);
 
     public abstract RestServerConfiguration getConfiguration ();
 
@@ -31,16 +34,23 @@ public abstract class LogRelayMemoryTarget implements LogRelayAppenderTarget {
     public String[] getLines () {
         final String[] copy;
         synchronized (index) {
+            if (indexAtLastGetLines.get() == index.get()) {
+                return lastLines.get();
+            }
             int i = index.get();
+            indexAtLastGetLines.set(index.get());
+            i--;
+            if (i == 0) return StringUtil.EMPTY_ARRAY;
             copy = new String[i > lines.length ? lines.length : i];
             if (i < lines.length) {
                 System.arraycopy(lines, 0, copy, 0, i);
             } else {
                 i %= lines.length;
-                // we have wrapped. copy lines from index to end, then from 0 to index
-                System.arraycopy(lines, i, copy, 0, lines.length-i);
-                if (lines.length - i > 0) System.arraycopy(lines, 0, copy, i, i);
+                // we have wrapped. copy lines from just after index to end, then from 0 to index
+                System.arraycopy(lines, i+1, copy, 0, lines.length-i-1);
+                if (lines.length - i > 0) System.arraycopy(lines, 0, copy, lines.length-i-1, i+1);
             }
+            lastLines.set(copy);
         }
         return copy;
     }
