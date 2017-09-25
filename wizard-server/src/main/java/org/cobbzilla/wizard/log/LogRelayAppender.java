f15c64dcc2cf73cc6019ca1569942f4bfa83530b
Jonathan Cobb <jonathan@kyuss.org>
2017-09-24 23:31:16 -0700

    cleanup pipes on stop or error, add comments, simplify

--- wizard-server/src/main/java/org/cobbzilla/wizard/log/LogRelayAppender.java
+++ wizard-server/src/main/java/org/cobbzilla/wizard/log/LogRelayAppender.java
@@ -1,6 +1,7 @@
 package org.cobbzilla.wizard.log;
 
 import ch.qos.logback.core.OutputStreamAppender;
+import lombok.Cleanup;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -10,73 +11,87 @@ import java.io.*;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.cobbzilla.util.daemon.ZillaRuntime.*;
+import static org.cobbzilla.util.reflect.ReflectionUtil.closeQuietly;
 import static org.cobbzilla.util.system.Sleep.sleep;
 
 @Slf4j
 public class LogRelayAppender<E> extends OutputStreamAppender<E> {
 
     @Getter @Setter private static volatile RestServerConfiguration config;
-    @Getter private PipedInputStream in = new PipedInputStream();
+
+    private PipedInputStream in;
+    private PipedOutputStream out;
 
     public static final long STARTUP_TIMEOUT = SECONDS.toMillis(30);
 
-    private void superStart() { super.start(); }
+    @Override public void stop() {
+        if (in != null) closeQuietly(in);
+        if (out != null) closeQuietly(out);
+        super.stop();
+    }
 
     @Override public void start() {
         final String simpleClass = getClass().getSimpleName();
         try {
-            setOutputStream(new PipedOutputStream(in));
-            daemon(() -> {
-                final long start = now();
-                while ((config == null || config.getApplicationContext() == null) && (now() - start < STARTUP_TIMEOUT)) {
-                    sleep(SECONDS.toMillis(1));
-                }
-                if (config == null) {
-                    log.warn(simpleClass+": RestServerConfiguration was never set, exiting");
-                    return;
-                }
-                if (config.getApplicationContext() == null) {
-                    log.warn(simpleClass+": RestServerConfiguration.applicationContext was never set, exiting");
-                    return;
-                }
-                final BufferedReader reader;
-                try {
-                    reader = new BufferedReader(new InputStreamReader(in));
-                } catch (Exception e) {
-                    log.warn(simpleClass+": error setting up reader, exiting");
-                    return;
-                }
-                final LogRelayAppenderConfig logRelayConfig = config.getLogRelay();
-                if (logRelayConfig == null) {
-                    log.warn(simpleClass+": no relayConfig was found, exiting");
-                    return;
-                }
-                final String relayTo = logRelayConfig.getRelayTo();
-                if (empty(relayTo)) {
-                    log.warn(simpleClass+": relayConfig was found, but relayTo was empty, exiting");
-                    return;
-                }
-                final LogRelayAppenderTarget relayTarget;
-                try {
-                    // cast shouldn't be required, but a compilation error occurs if we remove it
-                    relayTarget = (LogRelayAppenderTarget) config.getBean(relayTo);
-                    relayTarget.init(logRelayConfig.getParams());
-                } catch (Exception e) {
-                    log.warn(simpleClass+": error initializing relayTo ("+ relayTo +"), exiting: "+e);
-                    return;
-                }
-
-                superStart();
-                String line = null;
-                try {
-                    while ((line = reader.readLine()) != null) relayTarget.relay(line);
-                } catch (Exception e) {
-                    throw new IllegalStateException("LogRelayAppender: error relaying line ("+line+"), exiting: "+e);
-                }
-            });
+            in = new PipedInputStream();
+            out = new PipedOutputStream(in);
+            setOutputStream(out);
         } catch (IOException e) {
-            die("start: error setting up pipe: "+e, e);
+            stop();
+            throw new IllegalStateException("start: error setting up pipes: "+e, e);
         }
+        super.start();
+        daemon(() -> {
+            final long start = now();
+
+            // wait for config to get set (someone has to initialize us after spring is up)
+            while ((config == null || config.getApplicationContext() == null) && (now() - start < STARTUP_TIMEOUT)) {
+                sleep(SECONDS.toMillis(1));
+            }
+            if (config == null) {
+                log.warn(simpleClass+": RestServerConfiguration was never set, exiting");
+                stop(); return;
+            }
+            if (config.getApplicationContext() == null) {
+                log.warn(simpleClass+": RestServerConfiguration.applicationContext was never set, exiting");
+                stop(); return;
+            }
+
+            @Cleanup final BufferedReader reader;
+            try {
+                reader = new BufferedReader(new InputStreamReader(in));
+            } catch (Exception e) {
+                log.warn(simpleClass+": error setting up reader, exiting");
+                stop(); return;
+            }
+            final LogRelayAppenderConfig logRelayConfig = config.getLogRelay();
+            if (logRelayConfig == null) {
+                log.warn(simpleClass+": no relayConfig was found, exiting");
+                stop(); return;
+            }
+            final String relayTo = logRelayConfig.getRelayTo();
+            if (empty(relayTo)) {
+                log.warn(simpleClass+": relayConfig was found, but relayTo was empty, exiting");
+                stop(); return;
+            }
+            final LogRelayAppenderTarget relayTarget;
+            try {
+                // cast shouldn't be required, but a compilation error occurs if we remove it
+                relayTarget = (LogRelayAppenderTarget) config.getBean(relayTo);
+                relayTarget.init(logRelayConfig.getParams());
+            } catch (Exception e) {
+                log.warn(simpleClass+": error initializing relayTo ("+ relayTo +"), exiting: "+e);
+                stop(); return;
+            }
+
+            String line = null;
+            try {
+                while ((line = reader.readLine()) != null) relayTarget.relay(line);
+            } catch (Exception e) {
+                stop();
+                throw new IllegalStateException(simpleClass+": error relaying line ("+line+"), exiting: "+e);
+            }
+        });
     }
 
 }
--- wizard-server/src/main/java/org/cobbzilla/wizard/log/LogRelayMemoryTarget.java
+++ wizard-server/src/main/java/org/cobbzilla/wizard/log/LogRelayMemoryTarget.java
@@ -6,13 +6,18 @@ import org.cobbzilla.wizard.server.config.RestServerConfiguration;
 import javax.annotation.PostConstruct;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.cobbzilla.util.daemon.ZillaRuntime.now;
 
 public abstract class LogRelayMemoryTarget implements LogRelayAppenderTarget {
 
     private String[] lines;
     private final AtomicInteger index = new AtomicInteger(0);
     private final AtomicInteger indexAtLastGetLines = new AtomicInteger(-1);
+    private final AtomicLong lastGetLinesCall = new AtomicLong(-1);
     private final AtomicReference<String[]> lastLines = new AtomicReference<>(null);
 
     public abstract RestServerConfiguration getConfiguration ();
@@ -31,19 +36,38 @@ public abstract class LogRelayMemoryTarget implements LogRelayAppenderTarget {
         }
     }
 
+    // cache results for 2 seconds
+    public static final long CACHE_TIME = SECONDS.toMillis(2);
+
     public String[] getLines () {
         final String[] copy;
         synchronized (index) {
+            // only perform this calculation once per CACHE_TIME
+            final long now = now();
+            if (now - lastGetLinesCall.get() < CACHE_TIME && lastLines.get() != null) {
+                return lastLines.get();
+            }
+            lastGetLinesCall.set(now);
+
             int i = index.get();
+
+            // if nothing new has been written, return old value
             if (indexAtLastGetLines.get() == i) return lastLines.get();
             indexAtLastGetLines.set(i--);
+
+            // if nothing at all has been written, return nothing
             if (i == 0) return StringUtil.EMPTY_ARRAY;
+
+            // copy depends on whether or not we have wrapped
+
+            // allocate size. if we have not wrapped, size is smaller than lines.length
             copy = new String[i > lines.length ? lines.length : i];
             if (i < lines.length) {
+                // we have not wrapped. simply copy what we have written so far
                 System.arraycopy(lines, 0, copy, 0, i);
             } else {
+                // we have wrapped. copy lines from just after index to end, then copy from 0 to index (if any)
                 i %= lines.length;
-                // we have wrapped. copy lines from just after index to end, then from 0 to index
                 System.arraycopy(lines, i+1, copy, 0, lines.length-i-1);
                 if (lines.length - i > 0) System.arraycopy(lines, 0, copy, lines.length-i-1, i+1);
             }
