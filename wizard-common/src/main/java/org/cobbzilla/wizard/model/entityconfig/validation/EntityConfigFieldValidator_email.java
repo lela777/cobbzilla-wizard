95079aac003dafd550b363673d651bfc4066d4ff
Jovan Martinovic <jmartinovic@itekako.com>
2018-06-29 11:06:36 +0200

    changed regex

--- wizard-common/src/main/java/org/cobbzilla/wizard/model/entityconfig/validation/EntityConfigFieldValidator_email.java
+++ wizard-common/src/main/java/org/cobbzilla/wizard/model/entityconfig/validation/EntityConfigFieldValidator_email.java
@@ -7,15 +7,16 @@ import org.cobbzilla.wizard.validation.ValidationResult;
 import java.util.Locale;
 
 import static org.cobbzilla.util.daemon.ZillaRuntime.empty;
+import static org.cobbzilla.util.string.ValidationRegexes.EMAIL_PATTERN;
 
 public class EntityConfigFieldValidator_email implements EntityConfigFieldValidator {
-
-    final static String emailRegex = "^(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
 
     @Override public ValidationResult validate(Locale locale, EntityFieldConfig fieldConfig, Object value) {
         ValidationResult validation = new ValidationResult();
         final String val = empty(value) ? "" : value.toString().trim();
-        if (!val.matches(emailRegex)) validation.addViolation("err."+fieldConfig.getName()+".notEmail");
+        if (!EMAIL_PATTERN.matcher(val).find()) {
+            validation.addViolation("err."+fieldConfig.getName()+".notEmail");
+        }
         return validation;
     }
 
