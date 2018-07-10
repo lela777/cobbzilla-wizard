a9e0de66448e70267e7f70d3d900c3708356a509
Jonathan Cobb <jonathan@kyuss.org>
2018-07-10 14:32:12 -0700

    JsService now implements JsEngineFactory

--- wizard-server/src/main/java/org/cobbzilla/wizard/javascript/JavaScriptService.java
+++ wizard-server/src/main/java/org/cobbzilla/wizard/javascript/JavaScriptService.java
@@ -3,8 +3,9 @@ package org.cobbzilla.wizard.javascript;
 import lombok.Getter;
 import org.cobbzilla.util.javascript.JsEngine;
 import org.cobbzilla.util.javascript.JsEngineConfig;
+import org.cobbzilla.util.javascript.JsEngineFactory;
 
-public abstract class JavaScriptService {
+public abstract class JavaScriptService implements JsEngineFactory {
 
     public abstract JsEngineConfig getConfig();
 
